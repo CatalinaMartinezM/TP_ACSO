7f98750fa80edd1545b3be27f4931e325e7946bc
CatalinaMartinezM <cmartinezmartin@udesa.edu.ar>
2025-04-19 10:43:45 -0300

    No anda string_proc_list_add_node_asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -25,7 +25,7 @@ string_proc_list_create_asm:
     mov rdi, 16 ; guardo en rdi el tamaño de la lista
     call malloc ; la direccion de memoria donde empieza la lista se guarda en rax
 
-    test rax, rax ; rax == NULL
+    test rax, rax ; rax == NULL?
     je .end
 
     mov qword [rax], 0      ; list -> first = NULL
@@ -56,7 +56,46 @@ string_proc_node_create_asm:
     pop rbp
     ret
 
+; string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash)
 string_proc_list_add_node_asm:
+    push rbp
+    mov rbp, rsp
+    mov r8, rdi   ; list
+    mov r9b, sil  ; type
+    mov r10, rdx  ; hash
+    
+    movzx rdi, sil ; type
+    mov rsi, rdx   ; hash
+    call string_proc_node_create_asm
+
+    test rax, rax
+    je .end
+
+    mov r9, rax
+
+    test r8, r8 ; list == NULL?
+    je .end
+
+    ;mov r11, [r8 + 8] ; r11 = list -> last
+    ;test r11, r11           ; list -> last == NULL?
+    cmp qword [r8], 0
+    je .isEmpty
+
+    mov r11, [r8 + 8] ; r11 = list -> last
+    mov qword [rax + 8], r11 ; node -> previous = list -> last
+    mov qword [r11], rax     ; list -> last -> next = node;
+
+    mov qword [r11], rax ; list -> last  = node
+    jmp .end
+
+    .isEmpty
+    mov [r8], r9  ; list -> first = node
+    mov [r8 + 8], r9 ; list -> last  = node
+
+    .end
+    pop rbp
+    ret
+
 
 string_proc_list_concat_asm:
 
